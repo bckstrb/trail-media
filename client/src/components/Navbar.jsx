5eac54400ef62d2ea55a7eb06e7a07cb95da0348
Zach <z.d.jorgensen@gmail.com>
2022-11-20 15:12:08 -0700

    different pages created / added photos

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+export default function Navbar({ currentPage, handlePageChange }) {
+    return (
+
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        {/* <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#home" onClick = {() => handlePageChange("Home")}>Home</a>
+                        </li> */}
+                        <li class="nav-item">
+                            <a class="nav-link" href="#signup" onClick = {() => handlePageChange("SignUp")}>Sign Up</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#login" onClick = {() => handlePageChange("Login")}>Log in</a>
+                        </li>
+                       
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
