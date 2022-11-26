cf7dfcb183f31e698c61a66a963f4b03521c931b
Darin VanAusdal <darinv@iMac2017.local>
2022-11-25 19:05:04 -0700

    trail api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -27,4 +27,32 @@ const trailSearch = (lat, lon) => {
     });
 };
 
+// const trailSearch = (city) => {
+// const options = {
+//   method: 'GET',
+//   url: 'https://trailapi-trailapi.p.rapidapi.com/activity/',
+//   params: {
+//     lat: '34.1',
+//     limit: '25',
+//     lon: '-105.2',
+//     'q-city_cont': 'provo',
+//     'q-country_cont': 'United States',
+//     'q-state_cont': 'utah',
+//     radius: '25',
+//     'q-activities_activity_type_name_eq': 'hiking'
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': '0fa6ad6c03msh367b9d3b4228be3p1b8388jsnb44ad1ec5d1f',
+//     'X-RapidAPI-Host': 'trailapi-trailapi.p.rapidapi.com'
+//   }
+// };
+// console.log(options);
+
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
+// };
+
 export default trailSearch;
