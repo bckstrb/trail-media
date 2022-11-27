d9b3383e7a9226ec669cda7a0ebbaea564bd9cc8
Zach <z.d.jorgensen@gmail.com>
2022-11-27 14:25:35 -0700

    post page update

---
+++ client/src/components/PostList.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../styles/PostList.css';
+
+const PostList = ({ posts }) => {
+    if (!posts.length) {
+        return <h6> No Posts Yet </h6>
+    }
+
+    return (
+        <div>
+            {posts &&
+                posts.map((post) => (
+                    <div key={post._id} className="card mb-3 post-card">
+                        <h5 className="p-2 m-0">
+                            {post.postAuthor} <br />
+                            <span style={{ fontSize: '1rem' }}>
+                                created this post on {post.createdAt}
+                            </span>
+                        </h5>
+                        <div className="card-body bg-light p-2">
+                            <p>{post.postText}</p>
+                        </div>
+                    </div>
+                ))}
+        </div>
+    )
+}
+
+export default PostList;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,26 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/Profile.css";
 import { QUERY_SINGLE_PROFILE } from "../utils/queries";
 import { useQuery } from '@apollo/client';
 import Auth from '../utils/auth';
+import PostProfileList from "../components/PostProfileList";
 // import Link from 'react-router-dom';
 
 export default function Profile() {
   const user = Auth.getProfile();
   const profileId = user.data._id;
 
-  const { loading, data } = useQuery(QUERY_SINGLE_PROFILE, {
+  const  {data}  = useQuery(QUERY_SINGLE_PROFILE, {
     variables: { _id: profileId },
   });
 
   const profile = data?.profile || {};
+  const posts = data?.profile.posts || [];
   // LOOK AT MERN 24 FOR THOUGHT(POST) / COMMENTS EXAMPLE
-
-  // const postList = data?.posts || [];
-
-  // const [count, setCount] = useState('');
-  // const [lastName, setLastName] = useState('');
-  // const [email, setEmail] = useState('');
 
   return (
     <div className="container">
@@ -33,8 +29,6 @@ export default function Profile() {
             <h5> User: {profile.name} </h5>
             <br></br>
             <h5>Email: {profile.email}</h5>
-            <br></br>
-            <h5>Number of posts: {profile.posts} </h5>
           </div>
         </form>
       </div>
@@ -46,48 +40,9 @@ export default function Profile() {
 
         <div className="row posts">
           <div className="col-sm-4">
-            <div className="post">
-              <div>
-                <h5 className="card-title">Trail Name</h5>
-                <p className="card-text">User Post about the trail </p>
-                {/* {postList.map((matchup) => { From Mern MiniProj
-                  return (
-                    <li key={matchup._id}>
-                      <Link to={{ pathname: `/matchup/${matchup._id}` }}>
-                        {matchup.tech1} vs. {matchup.tech2}
-                      </Link>
-                    </li>
-                  );
-                })} */}
-              </div>
-            </div>
-          </div>
-
-          <div className="col-sm-4">
-            <div className="post">
-              <div>
-                <h5 className="card-title">Trail Name</h5>
-                <p className="card-text">User Post about the trail </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-sm-4">
-            <div className="post">
-              <div>
-                <h5 className="card-title">Trail Name</h5>
-                <p className="card-text">User Post about the trail </p>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-sm-4">
-            <div className="post">
-              <div>
-                <h5 className="card-title">Trail Name</h5>
-                <p className="card-text">User Post about the trail </p>
-              </div>
-            </div>
+                <PostProfileList 
+                    posts = {posts}
+                />
           </div>
         </div>
       </div>
